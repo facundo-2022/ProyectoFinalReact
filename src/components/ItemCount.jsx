47e29b6fd13edc0ce243c25c0bf7c397d247643b
fmetzler <fmetzler22@gmail.com>
2024-02-22 21:45:41 -0300

    cambios

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -11,15 +11,7 @@ export const ItemCount = () => {
     return (
         <div className="container mx-auto mt-8">
             <div className="flex items-center justify-center">
-                <div className="flex items-center">
-                    <button className="bg-yellow-300 hover:bg-yellow-500 text-white px-2 py-1 rounded" onClick={decrement}>
-                        <FaArrowDown />
-                    </button>
-                    <span className="text-lg font-bold mx-4">{count}</span>
-                    <button className="bg-yellow-300 hover:bg-yellow-500 text-white px-2 py-1 rounded" onClick={increment}>
-                        <FaArrowUp />
-                    </button>
-                </div>
+                
                 
                 <button className="bg-yellow-300 text-white px-2 py-1 rounded-md ml-4 hover:bg-yellow-500 focus:outline-none flex items-center" onClick={handleAddToCart}>
                     <FaShoppingCart className="mr-2" />
