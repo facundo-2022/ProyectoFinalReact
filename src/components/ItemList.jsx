7ecc153077c51ffb3702002df3bc571153ebf985
fmetzler <fmetzler22@gmail.com>
2024-03-07 19:42:13 -0300

    actualizacion de correccion de error

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -5,14 +5,14 @@ export const ItemList = ({ products, plantilla }) => {
     return (
         <>
             {
-                plantilla === 'Item'
-                    ?
-                    products.map(prod => <Item key={prod.id} product={prod} />)
-                    :
-                    products.map(prod => <ItemCart key={prod.id} product={prod} />)
-
+                products && products.length > 0 && (
+                    plantilla === 'Item'
+                        ?
+                        products.map(prod => <Item key={prod.id} product={prod} />)
+                        :
+                        products.map(prod => <ItemCart key={prod.id} product={prod} />)
+                )
             }
         </>
-
     )
 }
