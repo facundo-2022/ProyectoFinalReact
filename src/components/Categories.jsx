7ccdda0d7fd4061d7cabc572ecb38aa531c10add
fmetzler <fmetzler22@gmail.com>
2024-03-16 10:50:58 -0300

    cambios

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,28 +1,27 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faPizzaSlice, faMobileScreen } from "@fortawesome/free-solid-svg-icons"
-import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPizzaSlice, faMobileAlt } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 export const Categories = () => {
     return (
         <>
             <li className="text-white">
-                <Link to ={'/'}>
-                <button className=" bg-yellow-300 ease-in duration-300 text-white px-4 py-2 rounded flex items-center  ">
-                    <FontAwesomeIcon icon={faPizzaSlice} className="mr-2" />
-                   
-                </button>
+                <Link to={'/'}>
+                    <button className="bg-yellow-300 ease-in duration-300 text-white px-4 py-2 rounded flex items-center">
+                        <FontAwesomeIcon icon={faPizzaSlice} className="mr-2" size="lg" />
+                        <span>Pizza</span>
+                    </button>
                 </Link>
             </li>
             
             <li className="text-white">
-            <Link to ={'/Contacto'}>
-                <button className="bg-yellow-300 text-white px-4 py-2 rounded flex items-center">
-                    <FontAwesomeIcon icon={faMobileScreen} className="mr-2" />
-                   
-                </button>
+                <Link to={'/Contacto'}>
+                    <button className="bg-yellow-300 text-white px-4 py-2 rounded flex items-center">
+                        <FontAwesomeIcon icon={faMobileAlt} className="mr-2" size="lg" />
+                        <span>Contacto</span>
+                    </button>
                 </Link>
             </li>
-    
         </>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,37 @@
-import { CartWidget } from "./CartWidget"
-import { Categories } from "./Categories"
+import { useState } from 'react';
+import { CartWidget } from './CartWidget';
+import { Categories } from './Categories';
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <nav className=" p-6  mb-6" style={{ backgroundImage: `url('https://firebasestorage.googleapis.com/v0/b/pizzeria-mazzanta.appspot.com/o/cabezal.jpg?alt=media&token=bf2db2c0-49af-4708-a10a-cec25ec6cfd6')`, backgroundRepeat: 'no-repeat', backgroundSize:' cover' }}>
-            <div className="container mx-auto">
-                <div className="flex items-center justify-between ">
-                  
+        <nav className="p-6 mb-6 relative" style={{ backgroundImage: `url('https://firebasestorage.googleapis.com/v0/b/pizzeria-mazzanta.appspot.com/o/cabezal.jpg?alt=media&token=bf2db2c0-49af-4708-a10a-cec25ec6cfd6')`, backgroundRepeat: 'no-repeat', backgroundSize:' cover' }}>
+            <div className="container mx-auto relative z-10">
+                <div className="flex items-center justify-between">
                     <img src="https://firebasestorage.googleapis.com/v0/b/pizzeria-mazzanta.appspot.com/o/logo-removebg-preview.png?alt=media&token=330077c0-5a10-497b-bc5c-cc75a450f543" alt="Logo" className="h-16 w-auto ease-in duration-300" />
-                    <ul className="flex space-x-4 ">
+                    <h1 className="text-white text-2xl font-bold italic flex-grow text-center"> Pizzeria Mazzanta</h1>
+                    <div className="lg:hidden">
+                        <button onClick={toggleMenu} className="text-white focus:outline-none">
+                            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                {menuOpen ? (
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                                ) : (
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+                                )}
+                            </svg>
+                        </button>
+                    </div>
+                    <ul className={`lg:flex lg:space-x-4 ${menuOpen ? 'block' : 'hidden'} absolute top-full left-0 bg-white w-full p-4 lg:relative lg:w-auto z-20`} style={{ textAlign: 'center' }}>
                         <Categories />
                         <CartWidget />
                     </ul>
                 </div>
             </div>
         </nav>
-    )
-}
+    );
+};
